dcd117c8f1cc163a8608a16c9fe2c36f6f25fc7c
kangealme <kangealme@gmail.com>
2021-07-10 22:52:20 +0700

    Belajar Variabel dan jenis fungsi react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react'
+import Navbar from './Navbar';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Header />
       <h2>Hello World</h2>
     </div>
   );
---
+++ src/Variabel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const harga = 3000;
+harga = 4000;
+
+const Variabel = () => {
+    return (
+        <div>
+            <h3>Harga : {harga}</h3>
+        </div>
+    )
+}
+
+export default Variabel
